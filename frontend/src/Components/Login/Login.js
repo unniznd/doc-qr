266ac01c1c78b6cf9a97a247b2cdebe581d6d9d1
Shiyasmohd <shiyasjaseena124@gmail.com>
2022-02-04 21:45:05 +0530

    Login page responsive

--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -4,14 +4,16 @@ import './Login.css'
 function Login() {
   return (
     <div className="login">
-      <div className="heading"> <h1>Doc-QR</h1></div>
-      <h4>LOGIN</h4>
-      <form className="l-form">
-        <input type="text" className='l-input' placeholder='Username'/> <br/>
-        <input type="password" className='l-input' placeholder='Password'/>
-        <input type="submit" className='login-btn' value="LOGIN" />
-        <h6>Forgot Password? | Signup</h6>
-      </form>
+      <div className="login-wrap">
+        <div className="heading"> <h1>Doc-QR</h1></div>
+        <h4>LOGIN</h4>
+        <form className="l-form">
+          <input type="text" className='l-input' placeholder='Username'/> <br/>
+          <input type="password" className='l-input' placeholder='Password'/> <br/>
+          <input type="submit" className='login-btn' value="LOGIN" />
+          <h6>Forgot Password? | Signup</h6>
+        </form>
+      </div>
     </div>
   )
 }
